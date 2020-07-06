a2a73696359dfbe994bdd3332b6f05a8c902d9c7
Gustavo Gotardi <gustavo.gotardi@gmail.com>
2020-07-06 01:16:31 -0300

    Desáfio Tokio

---
+++ src/main/java/com/example/api/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.example.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.api.domain.Cliente;
+
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+
+}
---
+++ src/main/java/com/example/api/repository/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.example.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.api.domain.Endereco;
+
+
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
